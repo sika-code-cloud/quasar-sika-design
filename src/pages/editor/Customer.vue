e7ff3f63ef56f702bf289514405801743e26d704
daiqi <466608943@qq.com>
2020-12-17 01:22:31 +0800

    初始化quasar sika design

---
+++ src/pages/editor/Customer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <tinymce ref="editor" :key="tinymceFlag" v-model="goodsDescp" />
+  </div>
+</template>
+<script>
+import tinymce from 'Tinymce'
+export default {
+  components: { tinymce },
+  data() {
+    return {
+      tinymceFlag: 'editor',
+      goodsDescp: ''
+    }
+  }
+}
+</script>
