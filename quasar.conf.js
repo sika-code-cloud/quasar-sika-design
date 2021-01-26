646262d0135d1f1f21ab577d189e745bf2515177
daiqi <466608943@qq.com>
2021-01-27 00:01:11 +0800

    升级quasar

--- quasar.conf.js
+++ quasar.conf.js
@@ -155,9 +155,9 @@ module.exports = function (ctx) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'blog-admin-vue',
-        short_name: 'blog-admin-vue',
-        description: 'blog-admin-vue',
+        name: 'quasar-sika-design',
+        short_name: 'quasar-sika-design',
+        description: 'quasar-sika-design',
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
@@ -220,7 +220,7 @@ module.exports = function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'blog-admin-vue'
+        appId: 'quasar-sika-design'
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
--- src/boot/main.js
+++ src/boot/main.js
@@ -1,5 +1,4 @@
 import '@/mock/index'
-import '@/css/main.sass'
 import '@/assets/js/sc-common.js'
 import Vue from 'vue'
 import numeral from 'vue-numeral-filter'
